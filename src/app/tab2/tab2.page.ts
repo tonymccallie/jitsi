6748f9d968e000e4b72ea156ffef2366a83f8c9c
Tony McCallie <tonymccallie@gmail.com>
2019-11-12 15:29:43 -0700

    basic jitsi config

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import 'capacitor-jitsi-meet';
+const { Jitsi } = Plugins;
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+	selector: 'app-tab2',
+	templateUrl: 'tab2.page.html',
+	styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
 
-  constructor() {}
+	constructor() {
+		this.init();
+	}
 
+	async init() {
+		const result = await Jitsi.joinConference({
+			roomName: 'room1', // room identifier for the conference
+			url: 'https://meet.jit.si', // endpoint of the Jitsi Meet video bridge
+			channelLastN: '-1', // default -1. see config.js for more info on the last n feature
+			startWithAudioMuted: true, // start with audio muted
+			startWithVideoMuted: false // start with video muted
+		});
+		window.addEventListener('onConferenceJoined', () => {
+			console.log('onConferenceJoined');
+		});
+		window.addEventListener('onConferenceLeft', () => {
+			console.log('onConferenceLeft');
+		});
+	}
 }
